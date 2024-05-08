b6181fc1a3dda9514fd9122dc1ab06b6e4c704d8
LukasJ00 <96081832+LukasJ00@users.noreply.github.com>
2024-05-08 15:02:53 +0200

    produkt

--- wp-content/themes/Party-Pulse/functions.php
+++ wp-content/themes/Party-Pulse/functions.php
@@ -93,3 +93,7 @@ function enqueue_menu_icons_script() {
     wp_enqueue_script( 'menu-icons-script', get_template_directory_uri() . '/menu-icons.js', array(), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_menu_icons_script' );
+
+
+
+
