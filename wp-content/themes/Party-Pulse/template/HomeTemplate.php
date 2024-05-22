f52a8ef84d411b81c26bed16e593eb5367230601
LukasJ00 <96081832+LukasJ00@users.noreply.github.com>
2024-05-22 14:10:25 +0200

    new

--- wp-content/themes/Party-Pulse/template/HomeTemplate.php
+++ wp-content/themes/Party-Pulse/template/HomeTemplate.php
@@ -5,17 +5,17 @@ Template Name: Custom Homepage
 
 <?php get_header(); ?>
 
+<?php the_content(); ?>
 <div class="main-container">
-<div class="homepage-banner">
-<img src="<?php echo esc_url(home_url('/wp-content/uploads/2024/05/Eurovision-veckan.webp')); ?>" alt="Main-banner">
-</div>
-
 
 <?php echo do_shortcode('[sp_wpcarousel id="214"]'); ?>
 
+
 <?php echo do_shortcode('[sp_wpcarousel id="213"]'); ?>
 
+
 <?php echo do_shortcode('[sp_wpcarousel id="215"]'); ?>
+
 </div>
 
 <?php get_footer(); ?>
